ca610935f4edd290c9f426f587de93549fcb339e
iam-awaneesh <aw64an@gmail.com>
2025-12-24 22:31:06 +0530

    fix: update package versions and improve styling on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-      <div className="text-center">
+      <div className="text-center mt-20 text-2xl font-bold bg-yellow-200 p-10 rounded-lg">
         WorkShop page
     </div>
   );
